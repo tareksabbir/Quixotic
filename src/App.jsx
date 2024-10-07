58678aba83fb1fa63a0ff19474f8a1abc10da461
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-10-07 16:27:36 +0600

    Set up project structure with Tailwind CSS and ShadCN configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Button>Test Shadcn component</Button>
+    </div>
+  );
+};
+
+export default App;
