4db06d18d20f5def3902eee8c2523eba9f2f403a
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2024-10-09 01:09:23 +0600

    Completed Car Card section with responsive design
    Developed the Car Card section with fully responsive design.
    Created cards to display car details dynamically.
    Optimized layout for consistency across different screen sizes.

--- src/Shared/FakeData.jsx
+++ src/Shared/FakeData.jsx
@@ -6,7 +6,7 @@ function createRandomeCarList(){
         model:faker.vehicle.model(),
         type:faker.vehicle.type(),
         
-        image:'https://wroom.ru/i/news/15225img1.jpg',
+        image:'https://www.bentleyofgreenwich.com/imagetag/5638/2/l/Used-2021-Mercedes-Benz-AMG-GT-Black-Series.jpg',
         miles:1000,
         gearType:'Automatic',
         price:faker.finance.amount({min:4000, max:20000})
--- src/components/HomePageComponents/searchCarsCard/Cards.jsx
+++ src/components/HomePageComponents/searchCarsCard/Cards.jsx
@@ -1,13 +1,48 @@
 /* eslint-disable react/prop-types */
 
+import { Separator } from "@/components/ui/separator";
+import { LuFuel } from "react-icons/lu";
+import { IoSpeedometerOutline } from "react-icons/io5";
+import { GiGearStickPattern } from "react-icons/gi";
+import { MdOutlineOpenInNew } from "react-icons/md";
+
 const Cards = ({ car }) => {
   return (
-    <div className="border flex flex-col items-center justify-center p-4">
-      <div className="w-full h-[180px]  flex items-center justify-center">
-        <img src={car?.image} alt="" className="max-h-full object-contain" />
-      </div>
-      <div>
-        <h1 className="text-xl font-bold ">{car?.name}</h1>
+    <div className="rounded-xl bg-white border hover:shadow">
+      <h2 className="absolute m-2 text-white text-sm rounded-xl px-2  bg-black">New</h2>
+      <img
+        src={car?.image}
+        alt=""
+        className="max-h-full object-contain rounded-t-xl"
+      />
+      <div className=" p-4">
+        <h1 className="text-lg font-bold mb-2">{car?.name}</h1>
+        <Separator></Separator>
+        <div className="grid grid-cols-3 gap-4 my-3">
+          <div className="flex flex-col items-center ">
+            <LuFuel className="mb-2" />
+            <h2>{car?.miles}Miles</h2>
+          </div>
+          <div className="flex flex-col items-center ">
+            <IoSpeedometerOutline className="mb-2" />
+
+            <h2>{car?.fuelType} </h2>
+          </div>
+          <div className="flex flex-col items-center ">
+            <GiGearStickPattern className="mb-2" />
+
+            <h2>{car?.gearType}</h2>
+          </div>
+        </div>
+        <Separator></Separator>
+        <div className="flex justify-between items-center my-3 mx-2">
+          <h2 className="font-bold">$ {car?.price}</h2>
+          <h2 className="font-bold text-primary flex items-center">
+          <span>View Details</span>
+            <MdOutlineOpenInNew className="font-bold ml-2"/>
+            
+          </h2>
+        </div>
       </div>
     </div>
   );
